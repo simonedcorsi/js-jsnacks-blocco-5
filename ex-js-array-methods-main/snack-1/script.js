fc5fe1549cf2d95663d7ffd2fb9fa17e4881c9c9
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-01-27 12:05:47 +0100

    snack1

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -3,4 +3,7 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
-names.forEach
+names.forEach( (element) => {
+    console.log(element);
+
+});

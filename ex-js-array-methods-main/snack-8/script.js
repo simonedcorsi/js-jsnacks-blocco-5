f176cdaaed2b37e1f6194a15d6633a98fedaaed0
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-01-27 13:15:52 +0100

    snack 8 fail

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const students3 = students.find( (element) =>  )
+students3.class = '3C'
+console.log(students3)
